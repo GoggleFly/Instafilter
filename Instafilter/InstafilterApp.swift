76f7615e84d6fb5d5569e29b0f04217fb9166e30
David Ash <david.ash@ottid.co.uk>
2023-09-08 20:14:48 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by David Ash on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
